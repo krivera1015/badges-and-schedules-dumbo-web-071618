6655dd5bb20a74a0ac0766b3417fcc69e170a5f8
krivera1015 <krivera1015@gmail.com>
2018-07-11 22:36:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array_of_names)
+  array_of_names.each do |name|
+    return "Hello, my name is #{name}."
+  end
+end
+
+  
