5255b5e9653f3805a461a07ee24f5bdccf0de1d3
krivera1015 <krivera1015@gmail.com>
2018-07-12 00:12:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -31,7 +31,7 @@ def assign_rooms(array_of_speakers)
 end
 
 def printer(attendees)
-  assign_rooms.each do |rooms|
-    puts ""
+  batch_badge_creator(attendees).each do |attendee|
+    puts attendee
   end
 end
